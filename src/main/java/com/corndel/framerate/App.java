13132b61231e2cf65262e1d72562eb7e96729cd7
Charlie-robin <charlie.robin.richardson@gmail.com>
2024-10-28 11:01:54 +0000

    MVP :S

--- src/main/java/com/corndel/framerate/App.java
+++ src/main/java/com/corndel/framerate/App.java
@@ -1,5 +1,6 @@
 package com.corndel.framerate;
 
+import com.corndel.framerate.controllers.MovieController;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
@@ -8,31 +9,34 @@ import io.javalin.http.staticfiles.Location;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
 public class App {
-  public static void main(String[] args) {
-    var javalin = createApp();
-    javalin.start(8081);
-  }
+    public static void main(String[] args) {
+        var javalin = createApp();
+        javalin.start(8081);
+    }
 
-  public static Javalin createApp() {
-    var app = Javalin.create(
-        config -> {
-          config.staticFiles.add("/public", Location.CLASSPATH);
+    public static Javalin createApp() {
+        var app = Javalin.create(
+                config -> {
+                    config.staticFiles.add("/public", Location.CLASSPATH);
 
-          var resolver = new ClassLoaderTemplateResolver();
-          resolver.setPrefix("/templates/");
-          resolver.setSuffix(".html");
-          resolver.setTemplateMode("HTML");
+                    var resolver = new ClassLoaderTemplateResolver();
+                    resolver.setPrefix("/templates/");
+                    resolver.setSuffix(".html");
+                    resolver.setTemplateMode("HTML");
 
-          var engine = new TemplateEngine();
-          engine.setTemplateResolver(resolver);
+                    var engine = new TemplateEngine();
+                    engine.setTemplateResolver(resolver);
 
-          config.fileRenderer(new JavalinThymeleaf(engine));
+                    config.fileRenderer(new JavalinThymeleaf(engine));
+                });
+
+        app.get("/hello", ctx -> {
+            ctx.result("Hello, World!");
         });
 
-    app.get("/", ctx -> {
-      ctx.result("Hello, World!");
-    });
+        app.get("/", MovieController::getAllMovies);
+        app.get("/{movieId}", MovieController::getMovieById);
 
-    return app;
-  }
+        return app;
+    }
 }
